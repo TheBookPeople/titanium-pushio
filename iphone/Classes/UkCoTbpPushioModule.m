72129dd046a313fb1417be2418574a7e124f8cd8
William Griffiths <william@ycymro.com>
2014-10-17 15:52:39 +0100

    fix foobar in header

--- iphone/Classes/UkCoTbpPushioModule.m
+++ iphone/Classes/UkCoTbpPushioModule.m
@@ -5,12 +5,12 @@
  * Copyright (c) 2014 Your Company. All rights reserved.
  *
  * This file is part of Titatanium PushIO Module.
- * Foobar is free software: you can redistribute it and/or modify
+ * Titatanium PushIO Module is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  
- * Foobar is distributed in the hope that it will be useful,
+ * Titatanium PushIO Module is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
