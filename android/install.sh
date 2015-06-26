544964cac6dc61b8a9a55db6e79a07cdcec284fa
William Griffiths <william.griffiths@thebookpeople.co.uk>
2015-06-26 14:49:58 +0100

    update to version 1.3.0

--- android/install.sh
+++ android/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ant clean dist
 cd ./dist
-unzip uk.co.tbp.pushio-android-1.2.1.zip
+unzip uk.co.tbp.pushio-android-1.3.0.zip
 cp -a modules/android/uk.co.tbp.pushio ~/Library/Application\ Support/Titanium/modules/android
